350058f364cb596a8bcc5f0e4f03d0e91e767089
wanglilin0628 <hdu_lilinwang@163.com>
2020-12-14 21:40:10 +0800

    复原项目

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <start></start>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-import Start from '@/components/Start'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    // HelloWorld,
-    Start
+    HelloWorld
   }
 }
 </script>
