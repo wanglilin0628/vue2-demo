9a2b3eb1adb3fd08ce039315024451f3129aa6d4
wanglilin0628 <hdu_lilinwang@163.com>
2020-12-14 21:30:26 +0800

    https://github.com/wanglilin0628/vue2-demo.git

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <start></start>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Start from '@/components/Start'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    Start
   }
 }
 </script>
